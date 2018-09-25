2bcce94ee06e5a580a821dcb63e3171b1944813d
lqwsy <2567924198@qq.com>
2018-09-25 17:54:23 +0800

    change v2.0

--- src/main/Urlvalidation.go
+++ src/main/Urlvalidation.go
@@ -18,15 +18,15 @@ import (
 )
 
 const (
-	token = "aidogijaieoj293u98e08ha98yv740y9"
-	appID = "wx2a08c65012d0777d"
+	token       = "aidogijaieoj293u98e08ha98yv740y9"
+	appID       = "wx2a08c65012d0777d"
 	encodingKey = "lVI2zz4rR1qTQnoto90tlo3iEoh1uKfxOqACAuvhrAT"
 )
 
 var aesKey []byte
 
-func encodingAESKey2AESKey(encodingKey string)[]byte{
-	data,_ := base64.StdEncoding.DecodeString(encodingKey+"=")
+func encodingAESKey2AESKey(encodingKey string) []byte {
+	data, _ := base64.StdEncoding.DecodeString(encodingKey + "=")
 	return data
 }
 
@@ -89,7 +89,7 @@ func procRequest(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		if encryptType == "aes" {
 			fmt.Println("wx in safe model")
-			fmt.Println("msgSignature = ",msgSignature)
+			fmt.Println("msgSignature = ", msgSignature)
 			aesKey = encodingAESKey2AESKey(encodingKey)
 
 			encryptRequestBody := models.ParseEncryptRequestBody(r)
@@ -108,46 +108,45 @@ func procRequest(w http.ResponseWriter, r *http.Request) {
 			}
 
 			//Xml decoding
-			textRequestBody, _ := models.ParseEncryptTextRequestBody(plainData,appID)
-			fmt.Printf("user:%s\n content:%s\n",textRequestBody.FromUserName,textRequestBody.Content)
+			textRequestBody, _ := models.ParseEncryptTextRequestBody(plainData, appID)
+			fmt.Printf("user:%s\ncontent:%s\n", textRequestBody.FromUserName, textRequestBody.Content)
 
-			num,result := isContainKeyWord(textRequestBody.Content)
+			num, result := isContainKeyWord(textRequestBody.Content)
 			response := ""
-			if result == true{
-				fmt.Println("num = ",num)
-				response = "conversion "+string(num)+" integral"
-			}else{
-				fmt.Println("num = ",num)
+			if result == true {
+				fmt.Println("num = ", num)
+				response = "conversion " + strconv.Itoa(num) + " integral"
+			} else {
+				fmt.Println("num = ", num)
 				response = "do you want to conversion integral"
 			}
 
-			fmt.Println("response = ",response)
+			fmt.Println("response = ", response)
 			responseEncryptTextBody, _ := models.MakeEncryptResponseBody(textRequestBody.ToUserName,
 				textRequestBody.FromUserName,
 				response,
-				nonce,timestamp,appID,aesKey,token)
+				nonce, timestamp, appID, aesKey, token)
 			w.Header().Set("Content-Type", "text/xml")
 			fmt.Fprintf(w, string(responseEncryptTextBody))
 		}
 	}
 }
 
-func isContainKeyWord(word string)(int ,bool){
-	subString := substring(word,len([]rune(word))-2,len([]rune(word)))
-	numString := substring(word,0,len([]rune(word))-2)
-	if subString == "积分"{
-		num,err := strconv.Atoi(numString)
+func isContainKeyWord(word string) (int, bool) {
+	subString := substring(word, len([]rune(word))-2, len([]rune(word)))
+	numString := substring(word, 0, len([]rune(word))-2)
+	if subString == "积分" {
+		num, err := strconv.Atoi(numString)
 		if err != nil {
-			fmt.Println("not a number,num = ",num)
-			return num,false
-		}else{
-			fmt.Println("is number,num = ",num)
-			return num,true
+			fmt.Println("not a number,num = ", num)
+			return num, false
+		} else {
+			fmt.Println("is number,num = ", num)
+			return num, true
 		}
 	}
-	return 0,false
+	return 0, false
 }
-
 
 func substring(source string, start int, end int) string {
 	var r = []rune(source)
@@ -161,7 +160,7 @@ func substring(source string, start int, end int) string {
 		return source
 	}
 
-	return string(r[start : end])
+	return string(r[start:end])
 }
 
 func main() {
